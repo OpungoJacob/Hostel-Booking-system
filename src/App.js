c0132ecce63541e8ffea250c09ce41448a5c9d43
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-12 19:05:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from "./pages/Signup";
-import Login from "./pages/Login"
-import Home from "./pages/Home"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './style.css'
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
 export default function App(){
     return(
