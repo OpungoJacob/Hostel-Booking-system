fca8c33e87d0417aa3f6c92f0fdcf57bb26622ae
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-14 19:02:40 +0300

    Add files via upload

---
+++ nav-bar/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header/index"
+
+
+
+export default function App(){
+    return(
+        <div>
+           <Header /> 
+
+        </div>
+    )
+}
+
+
---
+++ nav-bar/src/components/Header/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../../assets/logo/logo.png"
+import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container, InputGroup,FormControl,Button}from 'react-bootstrap';
+
+
+export default function Header(){
+    return(
+        <div className="nav-contents">
+            <div className="navbar">
+                <img src={logo} className="logo" />
+                <h3 className="logo-text">Hostel Booking system</h3>
+                <span className="links">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Rooms</a>
+                    <a href="#">Contact</a>
+                </span>
+                
+        </div>
+        <div className="search">
+                    <Container>
+                        <InputGroup size="sm" className="mb-3" >
+                            <FormControl 
+                                size="sm"
+                                placeholder="Search for hostel"
+                                className="text-field"
+                            />
+                            <Button>Search</Button>
+                        </InputGroup>
+                    </Container>
+                </div>
+        </div>
+    )
+} 
