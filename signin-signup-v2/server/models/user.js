bdd92e17c154754461c4bed1c059463c26d41f7f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 16:43:04 +0300

    Update user.js

--- signin-signup-v2/server/models/user.js
+++ signin-signup-v2/server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const joi = require('joi');
-const passwordComplexity = require('joi-password-complexity')
+const passwordComplexity = require('joi-password-complexity');
 
 const userSchema = new mongoose.Schema({
     surname: {type: String, required: true},
