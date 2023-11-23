b11a103f10ab67e4e46ecb3b83d7b904ed2cc1ef
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 16:40:22 +0300

    Update index.js

--- signin-signup-v2/server/index.js
+++ signin-signup-v2/server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require ('express');
 const app = express();
 const cors = require("cors");
-const connection = require("./db")
+const connection = require("./db");
 const userRoutes = require('./routes/users')
 const authRoutes = require("./routes/auth")
 
