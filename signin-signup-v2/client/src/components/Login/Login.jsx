85614a5949afe804a180070b32c649f81e748e72
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 16:37:00 +0300

    Update Login.jsx

--- signin-signup-v2/client/src/components/Login/Login.jsx
+++ signin-signup-v2/client/src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import './styles.modules.css'
-import './login.css'
+import './login.css';
 import {Link} from 'react-router-dom'
 import {useState} from 'react'
 import axios from 'axios'
