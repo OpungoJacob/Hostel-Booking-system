3bd1e0b1fc2e3523d853786f073cbf080719fb97
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 16:35:04 +0300

    Update App.js

--- signin-signup-v2/client/src/App.js
+++ signin-signup-v2/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Navigate,Routes,Route} from 'react-router-dom';
-import Signup from "./components/Signup/Signup"
+import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login"
 import Main from "./components/Main/index"
 
