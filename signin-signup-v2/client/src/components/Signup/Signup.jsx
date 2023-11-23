0fa9c263960bf367c5f87a6571387f50d4fe60d8
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-23 16:39:06 +0300

    Update Signup.jsx

--- signin-signup-v2/client/src/components/Signup/Signup.jsx
+++ signin-signup-v2/client/src/components/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.modules.css';
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom';
 import {useState} from 'react';
 import axios from 'axios'
 
