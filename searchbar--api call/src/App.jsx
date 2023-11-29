a18263b3218f90da227e0f3d1e91a65d93bf4890
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-29 18:03:28 +0300

    Add files via upload

---
+++ searchbar--api call/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './Home'
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
